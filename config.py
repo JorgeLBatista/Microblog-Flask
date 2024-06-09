0e7f63b16e43bbe5df19d59791aeb80de19d8b36
Jorge L. Batista <jorgeluisbatista99@gmai.com>
2024-06-09 02:48:18 -0600

    Aqui se logro q funcionara el servidor de correo

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or ('your-secret-key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['elbati991023@gmail.com']
-    POSTS_PER_PAGE = 3
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 1025)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False # is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or None
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or None
+    ADMINS = ['esteeselcorreo@gmail.com']
+    POSTS_PER_PAGE = 25
